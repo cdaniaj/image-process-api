9e2fc40870c4f14d9c6cbf5a60e52ed5fe1e3b59
Carlos Daniel <carlosdaniel@Carloss-MacBook-Air.local>
2026-06-22 21:45:23 -0300

    fix: adding final consensus

--- data_extraction/extraction.py
+++ data_extraction/extraction.py
@@ -4,7 +4,7 @@ import os
 # Caminho absoluto que mapeia direto para o volume do Docker
 CSV_FOLDER = "/app/data_extraction/gerados"
 
-def fill_out_csv(patient_name, patient_id, img_path, area_mean, compactness_mean, perimeter_mean, concavity_mean, radius_mean):
+def fill_out_csv(patient_name, patient_id, img_path, area_mean, compactness_mean, perimeter_mean, concavity_mean, radius_mean, finalConsensus):
     os.makedirs(CSV_FOLDER, exist_ok=True)
     
     novo_dado = pd.DataFrame({
@@ -15,7 +15,8 @@ def fill_out_csv(patient_name, patient_id, img_path, area_mean, compactness_mean
         "compactness_mean": [round(compactness_mean, 3)],
         "perimeter_mean": [round(perimeter_mean, 3)],
         "concavity_mean": [round(concavity_mean, 3)],
-        "radius_mean": [round(radius_mean, 3)]
+        "radius_mean": [round(radius_mean, 3)],
+        "diagnosis": [finalConsensus]
     })
 
     csv_path = os.path.join(CSV_FOLDER, 'dataset_extraction.csv')
